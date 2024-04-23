2ae9d4a1e31fc82794e7144107aa3ab07d89e2f3
Slaven Konforta <slaven.konforta@gmail.com>
2024-04-23 12:02:29 +0200

    add shadcn UI

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,7 @@
 import Logo from '@/components/logo';
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -19,7 +21,14 @@ export default function Home() {
         <p className='max-w-[600px] text-2xl font-medium'>
           Use PetSoft to easily keep track of pets under your care. Get lifetime access for $299.
         </p>
-        <div className='mt-10'></div>
+        <div className='mt-10 space-x-3'>
+          <Button asChild>
+            <Link href='/signup'>Get started</Link>
+          </Button>
+          <Button asChild variant='secondary'>
+            <Link href='/login'>Log in</Link>
+          </Button>
+        </div>
       </div>
     </main>
   );
